85c77b2e9ce1550aaf722b112f9e8d8893c76afb
Daniel Lochner <daniel@daniellochner.com>
2021-12-08 13:28:24 +0200

    Rename to copyright notice editor

--- Scripts/Core/Editor/SSSCopyrightEditor.cs
+++ Scripts/Core/Editor/SSSCopyrightEditor.cs
@@ -7,7 +7,7 @@ namespace DanielLochner.Assets.SimpleScrollSnap
 {
     [CustomEditor(typeof(object), true)]
     [CanEditMultipleObjects]
-    public class SSSCopyrightEditor : CopyrightEditor
+    public class SSSCopyrightEditor : CopyrightNoticeEditor
     {
         public override string Product => "Simple Scroll-Snap";
         public override string CopyrightHolder => "Daniel Lochner";
